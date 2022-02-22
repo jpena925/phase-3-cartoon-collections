dd1ea1d66b8a4abc8cdc87514c7621323639726d
Jack Pena <penajackc@gmail.com>
2022-02-22 11:39:18 -0700

    commit tree 8a44326a2ba07189ed34edf9cb16cec0cc1f1f0a

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves arr
+  arr.each.with_index {|dwarf, index| puts "#{index + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet arr
+  arr.map {|word| "#{word.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls arr
+  longs = arr.filter{|word| word.length > 4}
+  longs.length > 0 ? true : false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese arr
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.length.times do |i|
+    if arr.include?(cheese_types[i])
+      return cheese_types[i]
+    end
+  end
+  return nil
 end
+
+
+find_the_cheese(['banana', 'cheddar'])
